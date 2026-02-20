8037ea8a0bb42f642eb1532125b818ed3bd93720
galvez-9e-ay2526 <galvez.jaden@obmontessori.edu.ph>
2026-02-20 11:28:10 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+//Function for Discount button
+function buttonDiscount (){
+    let Budget = document.getElementById(Budget).value;
+    //const Discount = .2;
+    let discountedPrice = Number(Budget * 0.2);
+
+    document.getElementById().innerHTML = '
+        <div class="container">
+    
+        
+        </div
+
+    ';
+}
